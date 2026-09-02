1770028502be3cc42d904886713d94be2e3481d1
sunjin jo <redsunjin@gmail.com>
2026-09-02 23:05:55 +0900

    Cache deterministic Explain assets

--- app_shell/terminal_shell_prototype/service-worker.js
+++ app_shell/terminal_shell_prototype/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "coquery-pwa-v2";
+const CACHE_NAME = "coquery-pwa-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -12,6 +12,8 @@ const APP_SHELL = [
   "./practice-result-visual.js",
   "./practice-query-flow.css",
   "./practice-query-flow.js",
+  "./practice-result-explain.css",
+  "./practice-result-explain.js",
   "./learning-path.css",
   "./learning-path.js",
   "./curriculum-expansion.js",
