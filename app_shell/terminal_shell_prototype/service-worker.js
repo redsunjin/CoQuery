2f69f118c3ee9fa9dc401cfade0d07adb48ace15
sunjin jo <redsunjin@gmail.com>
2026-09-02 22:34:35 +0900

    Cache BI visual and Query Graph assets

--- app_shell/terminal_shell_prototype/service-worker.js
+++ app_shell/terminal_shell_prototype/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "coquery-pwa-v1";
+const CACHE_NAME = "coquery-pwa-v2";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -8,6 +8,10 @@ const APP_SHELL = [
   "./onboarding.js",
   "./practice-focus.css",
   "./practice-focus.js",
+  "./practice-result-visual.css",
+  "./practice-result-visual.js",
+  "./practice-query-flow.css",
+  "./practice-query-flow.js",
   "./learning-path.css",
   "./learning-path.js",
   "./curriculum-expansion.js",
